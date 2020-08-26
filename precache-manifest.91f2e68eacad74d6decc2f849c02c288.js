984e78f6ab6b35de8613d3db64d956493057226e
viniciuslazzari <vinilazzari028@gmail.com>
2020-08-26 19:56:21 -0300

    Updates

---
+++ precache-manifest.91f2e68eacad74d6decc2f849c02c288.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609402fc69afa8d01275b462ff9ce57b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba8e1d4fe310728c153b",
+    "url": "/portfolio/static/css/main.6e9ae3fe.chunk.css"
+  },
+  {
+    "revision": "59650e915c4aa1edaaf3",
+    "url": "/portfolio/static/js/2.401e478f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.401e478f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba8e1d4fe310728c153b",
+    "url": "/portfolio/static/js/main.86112d41.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
